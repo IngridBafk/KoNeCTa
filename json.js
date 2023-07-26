75a15b4c321bef13dfe999e08ab8e1f050ce8ef5
nachoa88 <nacho.albiol88@gmail.com>
2023-07-26 11:46:07 +0200

    Update json.js
    Actualizado el script de JSON con el fetch.

--- json.js
+++ json.js
@@ -1,96 +1,17 @@
-const eventsJson = [  
-    {
-        eventID: 1,
-        eventName: "Graffiti", 
-        eventType: "Arte Urbano",
-        description: "TV Boy realizará un mural en defensa de los ataques homófobos ocurridos en el mes de Junio",
-        img: "./img/Graffitti.jpeg",
-        location: "Carrer Sant Pere més Baix, 70, 08003, Barcelona",
-        locationHtml: "https://goo.gl/maps/BeF9NBvyn8jmjHvM7",
-        startingDate: "12/08/2023",
-        finishingDate: "12/08/2023",
-        startingTime: "19:00",
-        finishingTime: "22:00"
-    },
-    {
-        eventID: 2,
-        eventName: "Exposició Parc Ciutadella",
-        eventType: "Cultural",
-        description: "Exposición de obras de diferentes artistas vinculadas al constante cambio de la cultura",
-        img: "./img/Exposicio.jpg",
-        location: "Passeig de Picasso, 21, 08003, Barcelona",
-        locationHtml: "https://goo.gl/maps/rqxjMRnSX2uufEFG8",
-        startingDate: "10/08/2023",
-        finishingDate: "13/08/2023",
-        startingTime: "10:00",
-        finishingTime: "20:00"
-    },
-    {
-        eventID: 3,
-        eventName: "Llums BCN",
-        eventType: "Art",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        img: "./img/Llums.png",
-        location: "Poblenou, 08018, Barcelona",
-        locationHtml: "https://goo.gl/maps/oFo1ft8AueihjdZR6",
-        startingDate: "03/02/2023",
-        finishingDate:  "05/02/2023",
-        startingTime: "18:00",
-        finishingTime: "23:00"
-    },
-    {
-        eventID: 4,
-        eventName: "La danza toma la calle",
-        eventType: "Danza",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        img: "./img/Danza.png",
-        location: "Plaza Orfila, 08030, Barcelona",
-        locationHtml: "https://goo.gl/maps/kkxYhubvdsLjJDAv7",
-        startingDate: "29/09/2023",
-        finishingDate:  "29/09/2023",
-        startingTime: "17:00",
-        finishingTime: "19:00"
-    },
-    { 
-        eventID: 5,
-        eventName: "Concierto Tarmac",
-        eventType: "Rock",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        img: "./img/rock.jpg",
-        location: "Plaça de Sant Pere, 1 08003, Barcelona",
-        locationHtml: "https://goo.gl/maps/UMaML2HW7xMUdePn9",
-        startingDate: "01/08/2023",
-        finishingDate:  "01/08/2023",
-        startingTime: "22:00",
-        finishingTime: "23.59"
-    },
-    { 
-        eventID: 6,
-        eventName: "Teatre a la fresca",
-        eventType: "Comedia",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        img: "./img/teatre.jpg",
-        location: "Plaça del Nen de la Rutlla, sn, 08041, Barcelona",
-        locationHtml: "https://goo.gl/maps/SDZMEerc1PDU4ckF9",
-        startingDate: "25/07/2023",
-        finishingDate:  "25/07/2023",
-        startingTime: "21.00",
-        finishingTime: "23.00"
-    }
-]
-
-    console.log(JSON.stringify(eventsJson))
-
-function details(id){
+function details(id) {
     alert(id)
 }
-document.addEventListener('DOMContentLoaded', function(){
-    eventsJson.forEach(event => {
-        
-        let cardContentHTML =`
+
+fetch("https://452a6db4-008b-4e47-b8ab-c89698a8b56c.mock.pstmn.io/events")
+    .then(res => res.json())
+    .then(objJson => {
+
+        objJson.forEach(event => {
+
+            let cardContentHTML = `
         <div class="row">        
             <div class="col-md-4">
-                <img src = "${event.img}" class="img-fluid rounded-start custom-img" alt="photo event">
+                <img src="${event.img}" class="img-fluid rounded-start custom-img" alt="photo event">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
@@ -98,16 +19,15 @@ document.addEventListener('DOMContentLoaded', function(){
                     <p class="card-text">${event.description}</p>
                     <a href=${event.locationHtml} target="_blank">${event.location}</a>
                     <p class="card-text"><small class="text-body-secondary">Date: ${event.startingDate} - ${event.finishingDate} Hour: ${event.startingTime} - ${event.finishingTime}</small></p>
-                    <a href='#' class="btn btn" onclick="details(${event.eventID})">Details</a>
+                    <a href='#' class="btn btn-primary" onclick="details(${event.eventID})">Details</a>
                 </div>
             </div>
-        </div>               
+        </div>
         `
+            let card = document.createElement('div');
+            card.className = 'card mb-3';
+            card.innerHTML = cardContentHTML;
 
-        let card= document.createElement('div');
-        card.className = 'card mb-3';
-        card.innerHTML = cardContentHTML;
-
-        document.getElementById('events').appendChild(card);
+            document.getElementById('events').appendChild(card);
+        })
     })
-})
